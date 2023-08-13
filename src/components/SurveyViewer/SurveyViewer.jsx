ebd4fe12bf58db98275f78ea436c7a2f120acd56
josafat <josa221001@gmail.com>
2023-08-12 21:22:14 -0600

    updt: estilizando componente y refactorizando

--- src/components/SurveyViewer/SurveyViewer.jsx
+++ src/components/SurveyViewer/SurveyViewer.jsx
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import {
+  Box,
+  Heading,
+  Text,
+  List,
+  ListItem,
+  Input,
+  Flex,
+  Button,
+  Checkbox,
+} from "@chakra-ui/react";
 
 const SurveyViewer = () => {
   const [surveyData, setSurveyData] = useState(null);
@@ -30,10 +41,30 @@ const SurveyViewer = () => {
     fetchSurveyData();
   }, [surveyId]);
 
-  const handleResponseChange = (questionIndex, response) => {
+  const handleResponseChange = (questionIndex, option) => {
+    setUserResponses((prevResponses) => {
+      const updatedResponses = { ...prevResponses };
+
+      if (!updatedResponses[questionIndex]) {
+        updatedResponses[questionIndex] = [];
+      }
+
+      if (updatedResponses[questionIndex].includes(option)) {
+        updatedResponses[questionIndex] = updatedResponses[
+          questionIndex
+        ].filter((response) => response !== option);
+      } else {
+        updatedResponses[questionIndex].push(option);
+      }
+
+      return updatedResponses;
+    });
+  };
+
+  const handleOpenEndedChange = (questionIndex, value) => {
     setUserResponses((prevResponses) => ({
       ...prevResponses,
-      [questionIndex]: response,
+      [questionIndex]: value,
     }));
   };
 
@@ -44,16 +75,18 @@ const SurveyViewer = () => {
           if (question.type === "OPEN_ENDED") {
             return {
               question: question.question,
-              response: [userResponses[questionIndex] || ""], // Almacenar la respuesta en un array
+              response: [userResponses[questionIndex] || ""],
             };
           } else {
             return {
               question: question.question,
-              response: userResponses[questionIndex] || "",
+              response: userResponses[questionIndex] || [],
             };
           }
         }
       );
+
+      console.log("User Survey Responses:", userSurveyResponses);
 
       const response = await axios.post(
         `http://localhost:9090/api/v1/save/${surveyId}`,
@@ -75,46 +108,57 @@ const SurveyViewer = () => {
   }
 
   return (
-    <div>
-      <h1>{surveyData.name}</h1>
-      <p>{surveyData.description}</p>
-      {surveyData.questions.map((question, questionIndex) => (
-        <div key={questionIndex}>
-          <h2>
-            Pregunta {questionIndex + 1}: {question.question}
-          </h2>
-          <p>Tipo de Pregunta: {question.type}</p>
-          {question.type !== "OPEN_ENDED" ? (
-            <ul>
-              {question.options.map((option, optionIndex) => (
-                <li key={optionIndex}>
-                  <input
-                    type={
-                      question.type === "SINGLE_CHOICE" ? "radio" : "checkbox"
-                    }
-                    value={option}
-                    checked={userResponses[questionIndex] === option}
-                    onChange={(e) =>
-                      handleResponseChange(questionIndex, e.target.value)
-                    }
-                  />
-                  {option}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <input
-              type="text"
-              value={userResponses[questionIndex] || ""}
-              onChange={(e) =>
-                handleResponseChange(questionIndex, e.target.value)
-              }
-            />
-          )}
-        </div>
-      ))}
-      <button onClick={handleSaveResponses}>Guardar Respuestas</button>
-    </div>
+    <Flex
+      align={"center"}
+      w={"full"}
+      justify={"center"}
+      minH={"max-content"}
+      mt={8}
+    >
+      <Box p={4}>
+        <Heading>{surveyData.name}</Heading>
+        <Text>{surveyData.description}</Text>
+        {surveyData.questions.map((question, questionIndex) => (
+          <Box key={questionIndex} mt={4}>
+            <Heading as="h2" size="md">
+              Pregunta {questionIndex + 1}: {question.question}
+            </Heading>
+            <Text>Tipo de Pregunta: {question.type}</Text>
+            {question.type === "OPEN_ENDED" ? (
+              <Input
+                type="text"
+                value={userResponses[questionIndex] || ""}
+                onChange={(e) =>
+                  handleOpenEndedChange(questionIndex, e.target.value)
+                }
+                mt={2}
+              />
+            ) : (
+              <List>
+                {question.options.map((option, optionIndex) => (
+                  <ListItem key={optionIndex}>
+                    <Checkbox
+                      value={option}
+                      isChecked={
+                        userResponses[questionIndex]?.includes(option) || false
+                      }
+                      onChange={() =>
+                        handleResponseChange(questionIndex, option)
+                      }
+                    >
+                      {option}
+                    </Checkbox>
+                  </ListItem>
+                ))}
+              </List>
+            )}
+          </Box>
+        ))}
+        <Button mt={4} colorScheme="blue" onClick={handleSaveResponses}>
+          Guardar Respuestas
+        </Button>
+      </Box>
+    </Flex>
   );
 };
 
